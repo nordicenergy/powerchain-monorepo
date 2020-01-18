79772bbdc75153bcb45abf16ed20f3d69925acd6
Nordic Energy <info@nordicenergy.io>
2020-01-18 12:43:42 +0200

    Upgrading

--- contracts/erc20/test/net_token.ts
+++ contracts/erc20/test/net_token.ts
@@ -1,22 +1,22 @@
-import { chaiSetup, constants, provider, txDefaults, web3Wrapper } from '@0x/contracts-test-utils';
-import { BlockchainLifecycle } from '@0x/dev-utils';
-import { BigNumber } from '@0x/utils';
-import { Web3Wrapper } from '@0x/web3-wrapper';
+import {chaiSetup, constants, provider, txDefaults, web3Wrapper} from '@powerchain/contracts-test-utils';
+import {BlockchainLifecycle} from '@powerchain/dev-utils';
+import {BigNumber} from '@powerchain/utils';
+import {Web3Wrapper} from '@powerchain/web3-wrapper';
 import * as chai from 'chai';
 
-import { ZRXTokenContract } from './wrappers';
+import {NETTokenContract} from './wrappers';
 
-import { artifacts } from './artifacts';
+import {artifacts} from './artifacts';
 
 chaiSetup.configure();
 const expect = chai.expect;
 const blockchainLifecycle = new BlockchainLifecycle(web3Wrapper);
 
-describe('ZRXToken', () => {
+describe('NETToken', () => {
     let owner: string;
     let spender: string;
     let MAX_UINT: BigNumber;
-    let zrxToken: ZRXTokenContract;
+    let netToken: NETTokenContract;
 
     before(async () => {
         await blockchainLifecycle.startAsync();
@@ -28,8 +28,8 @@ describe('ZRXToken', () => {
         const accounts = await web3Wrapper.getAvailableAddressesAsync();
         owner = accounts[0];
         spender = accounts[1];
-        zrxToken = await ZRXTokenContract.deployFrom0xArtifactAsync(
-            artifacts.ZRXToken,
+        netToken = await NETTokenContract.deployFrompowerchainArtifactAsync(
+            artifacts.NETToken,
             provider,
             txDefaults,
             artifacts,
@@ -44,34 +44,34 @@ describe('ZRXToken', () => {
     });
     describe('constants', () => {
         it('should have 18 decimals', async () => {
-            const decimals = new BigNumber(await zrxToken.decimals().callAsync());
+            const decimals = new BigNumber(await netToken.decimals().callAsync());
             const expectedDecimals = 18;
             expect(decimals).to.be.bignumber.equal(expectedDecimals);
         });
 
         it('should have a total supply of 1 billion tokens', async () => {
-            const totalSupply = new BigNumber(await zrxToken.totalSupply().callAsync());
+            const totalSupply = new BigNumber(await netToken.totalSupply().callAsync());
             const expectedTotalSupply = 1000000000;
             expect(Web3Wrapper.toUnitAmount(totalSupply, 18)).to.be.bignumber.equal(expectedTotalSupply);
         });
 
-        it('should be named 0x Protocol Token', async () => {
-            const name = await zrxToken.name().callAsync();
-            const expectedName = '0x Protocol Token';
+        it('should be named powerchain Protocol Token', async () => {
+            const name = await netToken.name().callAsync();
+            const expectedName = 'powerchain Protocol Token';
             expect(name).to.be.equal(expectedName);
         });
 
-        it('should have the symbol ZRX', async () => {
-            const symbol = await zrxToken.symbol().callAsync();
-            const expectedSymbol = 'ZRX';
+        it('should have the symbol NET', async () => {
+            const symbol = await netToken.symbol().callAsync();
+            const expectedSymbol = 'NET';
             expect(symbol).to.be.equal(expectedSymbol);
         });
     });
 
     describe('constructor', () => {
         it('should initialize owner balance to totalSupply', async () => {
-            const ownerBalance = await zrxToken.balanceOf(owner).callAsync();
-            const totalSupply = new BigNumber(await zrxToken.totalSupply().callAsync());
+            const ownerBalance = await netToken.balanceOf(owner).callAsync();
+            const totalSupply = new BigNumber(await netToken.totalSupply().callAsync());
             expect(totalSupply).to.be.bignumber.equal(ownerBalance);
         });
     });
@@ -79,14 +79,14 @@ describe('ZRXToken', () => {
     describe('transfer', () => {
         it('should transfer balance from sender to receiver', async () => {
             const receiver = spender;
-            const initOwnerBalance = await zrxToken.balanceOf(owner).callAsync();
+            const initOwnerBalance = await netToken.balanceOf(owner).callAsync();
             const amountToTransfer = new BigNumber(1);
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.transfer(receiver, amountToTransfer).sendTransactionAsync({ from: owner }),
+                await netToken.transfer(receiver, amountToTransfer).sendTransactionAsync({ from: owner }),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
-            const finalOwnerBalance = await zrxToken.balanceOf(owner).callAsync();
-            const finalReceiverBalance = await zrxToken.balanceOf(receiver).callAsync();
+            const finalOwnerBalance = await netToken.balanceOf(owner).callAsync();
+            const finalReceiverBalance = await netToken.balanceOf(receiver).callAsync();
 
             const expectedFinalOwnerBalance = initOwnerBalance.minus(amountToTransfer);
             const expectedFinalReceiverBalance = amountToTransfer;
@@ -95,7 +95,7 @@ describe('ZRXToken', () => {
         });
 
         it('should return true on a 0 value transfer', async () => {
-            const didReturnTrue = await zrxToken.transfer(spender, new BigNumber(0)).callAsync({
+            const didReturnTrue = await netToken.transfer(spender, new BigNumber(0)).callAsync({
                 from: owner,
             });
             expect(didReturnTrue).to.be.true();
@@ -104,30 +104,30 @@ describe('ZRXToken', () => {
 
     describe('transferFrom', () => {
         it('should return false if owner has insufficient balance', async () => {
-            const ownerBalance = await zrxToken.balanceOf(owner).callAsync();
+            const ownerBalance = await netToken.balanceOf(owner).callAsync();
             const amountToTransfer = ownerBalance.plus(1);
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.approve(spender, amountToTransfer).sendTransactionAsync({
+                await netToken.approve(spender, amountToTransfer).sendTransactionAsync({
                     from: owner,
                     gas: constants.MAX_TOKEN_APPROVE_GAS,
                 }),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
-            const didReturnTrue = await zrxToken.transferFrom(owner, spender, amountToTransfer).callAsync({
+            const didReturnTrue = await netToken.transferFrom(owner, spender, amountToTransfer).callAsync({
                 from: spender,
             });
             expect(didReturnTrue).to.be.false();
         });
 
         it('should return false if spender has insufficient allowance', async () => {
-            const ownerBalance = await zrxToken.balanceOf(owner).callAsync();
+            const ownerBalance = await netToken.balanceOf(owner).callAsync();
             const amountToTransfer = ownerBalance;
 
-            const spenderAllowance = await zrxToken.allowance(owner, spender).callAsync();
+            const spenderAllowance = await netToken.allowance(owner, spender).callAsync();
             const isSpenderAllowanceInsufficient = spenderAllowance.comparedTo(amountToTransfer) < 0;
             expect(isSpenderAllowanceInsufficient).to.be.true();
 
-            const didReturnTrue = await zrxToken.transferFrom(owner, spender, amountToTransfer).callAsync({
+            const didReturnTrue = await netToken.transferFrom(owner, spender, amountToTransfer).callAsync({
                 from: spender,
             });
             expect(didReturnTrue).to.be.false();
@@ -135,75 +135,75 @@ describe('ZRXToken', () => {
 
         it('should return true on a 0 value transfer', async () => {
             const amountToTransfer = new BigNumber(0);
-            const didReturnTrue = await zrxToken.transferFrom(owner, spender, amountToTransfer).callAsync({
+            const didReturnTrue = await netToken.transferFrom(owner, spender, amountToTransfer).callAsync({
                 from: spender,
             });
             expect(didReturnTrue).to.be.true();
         });
 
         it('should not modify spender allowance if spender allowance is 2^256 - 1', async () => {
-            const initOwnerBalance = await zrxToken.balanceOf(owner).callAsync();
+            const initOwnerBalance = await netToken.balanceOf(owner).callAsync();
             const amountToTransfer = initOwnerBalance;
             const initSpenderAllowance = MAX_UINT;
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.approve(spender, initSpenderAllowance).sendTransactionAsync({
+                await netToken.approve(spender, initSpenderAllowance).sendTransactionAsync({
                     from: owner,
                     gas: constants.MAX_TOKEN_APPROVE_GAS,
                 }),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.transferFrom(owner, spender, amountToTransfer).sendTransactionAsync({
+                await netToken.transferFrom(owner, spender, amountToTransfer).sendTransactionAsync({
                     from: spender,
                     gas: constants.MAX_TOKEN_TRANSFERFROM_GAS,
                 }),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
 
-            const newSpenderAllowance = await zrxToken.allowance(owner, spender).callAsync();
+            const newSpenderAllowance = await netToken.allowance(owner, spender).callAsync();
             expect(initSpenderAllowance).to.be.bignumber.equal(newSpenderAllowance);
         });
 
         it('should transfer the correct balances if spender has sufficient allowance', async () => {
-            const initOwnerBalance = await zrxToken.balanceOf(owner).callAsync();
-            const initSpenderBalance = await zrxToken.balanceOf(spender).callAsync();
+            const initOwnerBalance = await netToken.balanceOf(owner).callAsync();
+            const initSpenderBalance = await netToken.balanceOf(spender).callAsync();
             const amountToTransfer = initOwnerBalance;
             const initSpenderAllowance = initOwnerBalance;
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.approve(spender, initSpenderAllowance).sendTransactionAsync(),
+                await netToken.approve(spender, initSpenderAllowance).sendTransactionAsync(),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.transferFrom(owner, spender, amountToTransfer).sendTransactionAsync({
+                await netToken.transferFrom(owner, spender, amountToTransfer).sendTransactionAsync({
                     from: spender,
                     gas: constants.MAX_TOKEN_TRANSFERFROM_GAS,
                 }),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
 
-            const newOwnerBalance = await zrxToken.balanceOf(owner).callAsync();
-            const newSpenderBalance = await zrxToken.balanceOf(spender).callAsync();
+            const newOwnerBalance = await netToken.balanceOf(owner).callAsync();
+            const newSpenderBalance = await netToken.balanceOf(spender).callAsync();
 
             expect(newOwnerBalance).to.be.bignumber.equal(0);
             expect(newSpenderBalance).to.be.bignumber.equal(initSpenderBalance.plus(initOwnerBalance));
         });
 
         it('should modify allowance if spender has sufficient allowance less than 2^256 - 1', async () => {
-            const initOwnerBalance = await zrxToken.balanceOf(owner).callAsync();
+            const initOwnerBalance = await netToken.balanceOf(owner).callAsync();
             const amountToTransfer = initOwnerBalance;
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.approve(spender, amountToTransfer).sendTransactionAsync(),
+                await netToken.approve(spender, amountToTransfer).sendTransactionAsync(),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
             await web3Wrapper.awaitTransactionSuccessAsync(
-                await zrxToken.transferFrom(owner, spender, amountToTransfer).sendTransactionAsync({
+                await netToken.transferFrom(owner, spender, amountToTransfer).sendTransactionAsync({
                     from: spender,
                     gas: constants.MAX_TOKEN_TRANSFERFROM_GAS,
                 }),
                 constants.AWAIT_TRANSACTION_MINED_MS,
             );
 
-            const newSpenderAllowance = await zrxToken.allowance(owner, spender).callAsync();
+            const newSpenderAllowance = await netToken.allowance(owner, spender).callAsync();
             expect(newSpenderAllowance).to.be.bignumber.equal(0);
         });
     });
--- packages/migrations/src/utils/token_info.ts
+++ packages/migrations/src/utils/token_info.ts
@@ -1,16 +1,16 @@
-import { BigNumber, NULL_BYTES } from '@0x/utils';
+import {BigNumber, NULL_BYTES} from '@powerchain/utils';
 
-import { ERC20Token, ERC721Token } from '../types';
+import {ERC20Token, ERC721Token} from '../types';
 
 export const etherTokenByChain: { [chainId: number]: { address: string } } = {
     3: {
-        address: '0xc778417e063141139fce010982780140aa0cd5ab',
+        address: 'nordicenergy0x4D0A4C762BD7f742096DAAF5911dcf9C94b9ea95',
     },
     4: {
-        address: '0xc778417e063141139fce010982780140aa0cd5ab',
+        address: 'nordicenergy0x4D0A4C762BD7f742096DAAF5911dcf9C94b9ea95',
     },
     42: {
-        address: '0xd0a1e359811322d97991e03f863a0c30c2cf029c',
+        address: 'powerchain0x5C2769c2e8932e643F5E69C970D405c8A9d68621',
     },
     1337: {
         address: '',
@@ -18,8 +18,8 @@ export const etherTokenByChain: { [chainId: number]: { address: string } } = {
 };
 export const erc20TokenInfo: ERC20Token[] = [
     {
-        name: 'Augur Reputation Token',
-        symbol: 'REP',
+        name: 'PowerChain Token',
+        symbol: 'PWRC',
         decimals: new BigNumber(18),
         ipfsHash: NULL_BYTES,
         swarmHash: NULL_BYTES,
@@ -46,8 +46,8 @@ export const erc20TokenInfo: ERC20Token[] = [
         swarmHash: NULL_BYTES,
     },
     {
-        name: 'Melon Token',
-        symbol: 'MLN',
+        name: 'Nordic Energy Token',
+        symbol: 'NET',
         decimals: new BigNumber(18),
         ipfsHash: NULL_BYTES,
         swarmHash: NULL_BYTES,
@@ -56,7 +56,7 @@ export const erc20TokenInfo: ERC20Token[] = [
 
 export const erc721TokenInfo: ERC721Token[] = [
     {
-        name: '0xen ERC721',
-        symbol: '0xen',
+        name: 'powerchain ERC721',
+        symbol: 'powerchain',
     },
 ];
